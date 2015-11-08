18a2d773c797be00699d51818b2d42bb8ac85ed7
Taro Minowa Higepon <higepon@twitter.com>
2015-11-08 05:17:11 -0800

    Initial Commit

---
+++ UICollectionView-Real-Examples/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UICollectionView-Real-Examples
+//
+//  Created by Taro Minowa on 11/8/15.
+//  Copyright © 2015 Higepon Taro Minowa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ UICollectionView-Real-Examples/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  UICollectionView-Real-Examples
+//
+//  Created by Taro Minowa on 11/8/15.
+//  Copyright © 2015 Higepon Taro Minowa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
