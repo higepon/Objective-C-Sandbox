b5c327a0c191f077636623325e70a7c8f0ad6283
Taro Minowa Higepon <higepon@twitter.com>
2015-11-08 05:34:35 -0800

    Add empty UICollectionVC.

--- UICollectionView-Real-Examples/PhotoCollectionViewController.h
+++ UICollectionView-Real-Examples/PhotoCollectionViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  PhotoCollectionViewController.h
 //  UICollectionView-Real-Examples
 //
 //  Created by Taro Minowa on 11/8/15.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface PhotoCollectionViewController : UICollectionViewController
 
 @end
-
